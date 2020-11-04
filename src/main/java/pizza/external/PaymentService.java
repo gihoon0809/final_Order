ebc34310b3c750777ca5fffcfe7c314100aa9a37
gihoon0809 <67869000+gihoon0809@users.noreply.github.com>
2020-11-04 20:55:53 +0900

    Update PaymentService.java

--- src/main/java/pizza/external/PaymentService.java
+++ src/main/java/pizza/external/PaymentService.java
@@ -11,7 +11,8 @@ import java.util.Date;
 // checkpoint4. req/res LDH 202011041030
 // local test를 위해 url 정보 url="http://localhost:8082 로 수정 (localhost, port)
 //@FeignClient(name="Payment", url="http://Payment:8080")
-@FeignClient(name="Payment", url="http://localhost:8082")
+//@FeignClient(name="Payment", url="http://localhost:8082")
+@FeignClient(name="payment", url="${api.url.payment}")
 public interface PaymentService {
 
     @RequestMapping(method= RequestMethod.POST, path="/payments")
